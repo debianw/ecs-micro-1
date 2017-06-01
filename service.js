4cc8c3bd53388582ebd1374802f207b2d1a17920
Waly <debianw@gmail.com>
2017-06-01 13:45:06 -0600

    small change

--- service.js
+++ service.js
@@ -1,9 +1,13 @@
+const os = require('os');
 const express = require('express');
 const app = express();
 const port = 9000;
 
 app.get('/', (req, res) => {
-  res.json({ instance: 'hello micro-1 , waly, test #2'});
+  res.json({ 
+    instance: 'hello micro-1 , waly, test #3',
+    hostname: os.hostname()
+  });
 });
 
 app.listen(port, () => {
